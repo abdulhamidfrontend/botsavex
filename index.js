e65e446c467bbaf65a2e3db203b416e428ccf8f0
712 <khamidullayew@gmail.com>
2025-07-14 04:55:15 +0500

    "aa"

--- index.js
+++ index.js
@@ -4,27 +4,24 @@ const TelegramBot = require("node-telegram-bot-api");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Tokenni bevosita yozamiz (agar .env ishlamayotgan bo‘lsa)
+// Token
 const token = "8110277054:AAGlsNUWbpYJBKJnseAYuOP5UPGwozkbi1M";
+const WEBHOOK_URL = `https://botsavex.onrender.com`; // ✅ ortiqcha slash yo‘q
 
-// Botni webhook rejimida ishga tushiramiz
 const bot = new TelegramBot(token);
-bot.setWebHook(`https://<RENDER-APP-NOMI>.onrender.com/bot${token}`);
+bot.setWebHook(`${WEBHOOK_URL}/bot${token}`);
 
-// Telegram webhookni qabul qiladigan endpoint
 app.use(express.json());
 app.post(`/bot${token}`, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
 
-// /start komandasi
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, "Salom! Webhook orqali ishga tushdim 🚀");
 });
 
-// Oddiy matn
 bot.on("message", (msg) => {
   if (msg.text !== "/start") {
     bot.sendMessage(msg.chat.id, `Siz yozdingiz: ${msg.text}`);
@@ -32,5 +29,5 @@ bot.on("message", (msg) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server ishga tushdi: ${port}`);
+  console.log(`✅ Server ishga tushdi: ${port}`);
 });
